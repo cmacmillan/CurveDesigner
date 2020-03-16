81b728593bd9ffee15092a632491018b9511b8ed
Chase MacMillan <chasemacmillan@hotmail.com>
2020-03-15 22:30:11 -0700

    fixed stupid guispace issue

--- Assets/NewUI/SizeCircleComposite.cs
+++ Assets/NewUI/SizeCircleComposite.cs
@@ -13,31 +13,17 @@ namespace Assets.NewUI
         private SizeCircleEdgePointPositionProvider _point;
         private SizeCircleComposite _owner;
 
-        public SizeCurveEdgeClickCommand(FloatDistanceValue ring, SizeCircleEdgePointPositionProvider point,SizeCircleComposite owner)
+        public SizeCurveEdgeClickCommand(FloatDistanceValue ring, SizeCircleEdgePointPositionProvider point, SizeCircleComposite owner)
         {
             this._owner = owner;
             this._ring = ring;
             this._point = point;
         }
-        Vector2 GetClosestPoint(Vector2 lineDirection, Vector2 lineOrigin, Vector2 point)
-        {
-            lineDirection = lineDirection.normalized;
-            Vector2 normal = new Vector2(-lineDirection.y,lineDirection.x).normalized;
-            if (lineDirection.x == 0)
-                throw new NotImplementedException();
-            var rs = lineDirection.y / lineDirection.x;
-            var rt = normal.y / normal.x;
-            var x = (-lineOrigin.y - rt * point.x + point.y+rs*lineOrigin.x) /(rs-rt);
-            var y = rs * (x - lineOrigin.x) + lineOrigin.y;
-            return new Vector2(x, y);
-        }
         void Set()
         {
             Camera sceneCam = UnityEditor.SceneView.lastActiveSceneView.camera;
             Vector2 mousePos = Event.current.mousePosition;
-            mousePos.x = mousePos.x/Screen.width;
-            mousePos.y = 1.0f-mousePos.y/Screen.height;
-            Ray cursorRay = sceneCam.ViewportPointToRay(mousePos);
+            Ray cursorRay = sceneCam.ScreenPointToRay(GUITools.GuiSpaceToScreenSpace(mousePos));
             _owner.GetPositionAndForward(out var position, out var forward);
             Plane circlePlane = new Plane(forward,position);
             if (circlePlane.Raycast(cursorRay,out float enter))
