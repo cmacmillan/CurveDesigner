400500da9ab3f1cc8524f2794714876d4c816c04
Chase MacMillan <chasemacmillan@hotmail.com>
2020-03-15 21:59:58 -0700

    size dragging working

--- Assets/NewUI/SizeCircleComposite.cs
+++ Assets/NewUI/SizeCircleComposite.cs
@@ -9,19 +9,53 @@ namespace Assets.NewUI
 {
     public class SizeCurveEdgeClickCommand : IClickCommand
     {
+        private FloatDistanceValue _ring;
+        private SizeCircleEdgePointPositionProvider _point;
+        private SizeCircleComposite _owner;
+
+        public SizeCurveEdgeClickCommand(FloatDistanceValue ring, SizeCircleEdgePointPositionProvider point,SizeCircleComposite owner)
+        {
+            this._owner = owner;
+            this._ring = ring;
+            this._point = point;
+        }
+        Vector2 GetClosestPoint(Vector2 lineDirection, Vector2 lineOrigin, Vector2 point)
+        {
+            lineDirection = lineDirection.normalized;
+            Vector2 normal = new Vector2(-lineDirection.y,lineDirection.x).normalized;
+            if (lineDirection.x == 0)
+                throw new NotImplementedException();
+            var rs = lineDirection.y / lineDirection.x;
+            var rt = normal.y / normal.x;
+            var x = (-lineOrigin.y - rt * point.x + point.y+rs*lineOrigin.x) /(rs-rt);
+            var y = rs * (x - lineOrigin.x) + lineOrigin.y;
+            return new Vector2(x, y);
+        }
+        void Set()
+        {
+            Camera sceneCam = UnityEditor.SceneView.lastActiveSceneView.camera;
+            Vector2 mousePos = Event.current.mousePosition;
+            mousePos.x = mousePos.x/Screen.width;
+            mousePos.y = 1.0f-mousePos.y/Screen.height;
+            Ray cursorRay = sceneCam.ViewportPointToRay(mousePos);
+            _owner.GetPositionAndForward(out var position, out var forward);
+            Plane circlePlane = new Plane(forward,position);
+            if (circlePlane.Raycast(cursorRay,out float enter))
+                _ring.value = Vector3.Distance(cursorRay.GetPoint(enter),position);
+        }
         public void ClickDown(Vector2 mousePos)
         {
-            throw new NotImplementedException();
+            Set();
         }
 
         public void ClickDrag(Vector2 mousePos, Curve3D curve, ClickHitData clicked)
         {
-            throw new NotImplementedException();
+            Set();
         }
 
         public void ClickUp(Vector2 mousePos)
         {
-            throw new NotImplementedException();
+            Set();
         }
     }
     public class SizeCircleEdgePointPositionProvider : IPositionProvider
@@ -64,11 +98,10 @@ namespace Assets.NewUI
             for (int i = 0; i < ringPointCount; i++)
             {
                 var edgePointProvider = new SizeCircleEdgePointPositionProvider(_point,i,curve);
-                var clickCommmand = new SizeCurveEdgeClickCommand();
+                var clickCommmand = new SizeCurveEdgeClickCommand(value,edgePointProvider,this);
                 ringPoints.Add(new PointComposite(this,edgePointProvider,PointTextureType.diamond,clickCommmand,new Color(.6f,.6f,.9f)));
             }
         }
-
         public Vector3 Position {
             get {
                 GetPositionAndForward(out Vector3 position, out Vector3 forward);
@@ -83,7 +116,7 @@ namespace Assets.NewUI
             set { _point.Distance = value; }
         }
 
-        private void GetPositionAndForward(out Vector3 position, out Vector3 forward)
+        public void GetPositionAndForward(out Vector3 position, out Vector3 forward)
         {
             var point = _positionCurve.GetPointAtDistance(_point.Distance);
             position = point.position;
