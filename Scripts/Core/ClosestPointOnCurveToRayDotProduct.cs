7518bdb2da1bd940f7f25ec040bcd48519829d56
Chase MacMillan <chasemacmillan@hotmail.com>
2021-12-20 22:48:08 -0800

    more accurate cursor calculation working

--- Scripts/Core/ClosestPointOnCurveToRayDotProduct.cs
+++ Scripts/Core/ClosestPointOnCurveToRayDotProduct.cs
@@ -35,60 +35,60 @@ namespace ChaseMacMillan.CurveDesigner
                 p1 -= ray.origin;
                 p2 -= ray.origin;
                 p3 -= ray.origin;
-                Coefs coefs = GetCoefs(p0,p1,p2,p3,ray.direction);
                 for (int i = 0; i < initialPointCount; i++)
                 {
-                    float t =(i+1)/(float)(initialPointCount+1);
-                    float t2;
-                    Vector3 pos;
-                    float k;
+                    double t =(i+1)/(double)(initialPointCount+1);
                     for (int j = 0; j < newtonsIterations; j++)
                     {
-                        pos = GetPos(p0,p1,p2,p3,t);
-                        k = pos.magnitude;
-                        t2 = t * t;
-                        float num = (3 * coefs.t3 * t2 + 2 * coefs.t2 * t + coefs.t1)/k;//k can cancel, so remove
-                        float denom = (6 * coefs.t3 * t + 2 * coefs.t2)/k;
-                        t = t - (num / denom);
+                        deriv(p0, p1, p2, p3, ray.direction, t, out double firstDeriv, out double secondDeriv);
+                        t = t - firstDeriv / secondDeriv;
                     }
-                    t = Mathf.Clamp01(t);
-                    pos = GetPos(p0, p1, p2, p3, t);
-                    k = pos.magnitude;
-                    t2 = t * t;
-                    float t3 = t * t * t;
-                    float fitness = (coefs.t3 * t3 + coefs.t2 * t2 + coefs.t1 * t + coefs.c) / k;
+                    t = (t > 1 ? 1 : (t < 0 ? 0 : t));
+                    Vector3 pos = GetPos(p0, p1, p2, p3, (float)t);
+                    float fitness = Vector3.Dot(ray.direction,pos.normalized);
                     if (fitness > bestFitness)
                     {
                         bestFitness = fitness;
                         bestSegmentIndex = segmentIndex;
-                        bestTime = t;
+                        bestTime = (float)t;
                     }
                 }
             }
             resultSegmentIndex = bestSegmentIndex;
             resultTime = bestTime;
         }
+        private static void deriv(Vector3 p0,Vector3 p1,Vector3 p2,Vector3 p3,Vector3 r,double t, out double firstDeriv, out double secondDeriv)
+        {
+            double rx = r.x;
+            double ry = r.y;
+            double rz = r.z;
+            double t2 = t * t;
+            double t3 = t * t * t;
+            double iT = 1 - t;
+            double iT2 = iT * iT;
+            double iT3 = iT * iT * iT;
+            double p0x = p0.x;
+            double p0y = p0.y;
+            double p0z = p0.z;
+            double p1x = p1.x;
+            double p1y = p1.y;
+            double p1z = p1.z;
+            double p2x = p2.x;
+            double p2y = p2.y;
+            double p2z = p2.z;
+            double p3x = p3.x;
+            double p3y = p3.y;
+            double p3z = p3.z;
+            firstDeriv = (rz * (3 * p3z * t2 - 3 * p2z * t2 + 6 * p2z * iT * t - 6 * p1z * iT * t + 3 * p1z * iT2 - 3 * p0z * iT2) + ry * (3 * p3y * t2 - 3 * p2y * t2 + 6 * p2y * iT * t - 6 * p1y * iT * t + 3 * p1y * iT2 - 3 * p0y * iT2) + rx * (3 * p3x * t2 - 3 * p2x * t2 + 6 * p2x * iT * t - 6 * p1x * iT * t + 3 * p1x * iT2 - 3 * p0x * iT2)) / sqrt(sqr(p3z * t3 + 3 * p2z * iT * t2 + 3 * p1z * iT2 * t + p0z * iT3) + sqr(p3y * t3 + 3 * p2y * iT * t2 + 3 * p1y * iT2 * t + p0y * iT3) + sqr(p3x * t3 + 3 * p2x * iT * t2 + 3 * p1x * iT2 * t + p0x * iT3)) - ((rz * (p3z * t3 + 3 * p2z * iT * t2 + 3 * p1z * iT2 * t + p0z * iT3) + ry * (p3y * t3 + 3 * p2y * iT * t2 + 3 * p1y * iT2 * t + p0y * iT3) + rx * (p3x * t3 + 3 * p2x * iT * t2 + 3 * p1x * iT2 * t + p0x * iT3)) * (2 * (3 * p3z * t2 - 3 * p2z * t2 + 6 * p2z * iT * t - 6 * p1z * iT * t + 3 * p1z * iT2 - 3 * p0z * iT2) * (p3z * t3 + 3 * p2z * iT * t2 + 3 * p1z * iT2 * t + p0z * iT3) + 2 * (3 * p3y * t2 - 3 * p2y * t2 + 6 * p2y * iT * t - 6 * p1y * iT * t + 3 * p1y * iT2 - 3 * p0y * iT2) * (p3y * t3 + 3 * p2y * iT * t2 + 3 * p1y * iT2 * t + p0y * iT3) + 2 * (3 * p3x * t2 - 3 * p2x * t2 + 6 * p2x * iT * t - 6 * p1x * iT * t + 3 * p1x * iT2 - 3 * p0x * iT2) * (p3x * t3 + 3 * p2x * iT * t2 + 3 * p1x * iT2 * t + p0x * iT3))) / (2 * System.Math.Pow(sqr(p3z * t3 + 3 * p2z * iT * t2 + 3 * p1z * iT2 * t + p0z * iT3) + sqr(p3y * t3 + 3 * p2y * iT * t2 + 3 * p1y * iT2 * t + p0y * iT3) + sqr(p3x * t3 + 3 * p2x * iT * t2 + 3 * p1x * iT2 * t + p0x * iT3), (3.0 / 2.0)));
+            secondDeriv = (rz * (6 * p3z * t - 12 * p2z * t + 6 * p1z * t + 6 * p2z * iT - 12 * p1z * iT + 6 * p0z * iT) + ry * (6 * p3y * t - 12 * p2y * t + 6 * p1y * t + 6 * p2y * iT - 12 * p1y * iT + 6 * p0y * iT) + rx * (6 * p3x * t - 12 * p2x * t + 6 * p1x * t + 6 * p2x * iT - 12 * p1x * iT + 6 * p0x * iT)) / sqrt(sqr(p3z * t3 + 3 * p2z * iT * t2 + 3 * p1z * iT2 * t + p0z * iT3) + sqr(p3y * t3 + 3 * p2y * iT * t2 + 3 * p1y * iT2 * t + p0y * iT3) + sqr(p3x * t3 + 3 * p2x * iT * t2 + 3 * p1x * iT2 * t + p0x * iT3)) - ((rz * (p3z * t3 + 3 * p2z * iT * t2 + 3 * p1z * iT2 * t + p0z * iT3) + ry * (p3y * t3 + 3 * p2y * iT * t2 + 3 * p1y * iT2 * t + p0y * iT3) + rx * (p3x * t3 + 3 * p2x * iT * t2 + 3 * p1x * iT2 * t + p0x * iT3)) * (2 * sqr(3 * p3z * t2 - 3 * p2z * t2 + 6 * p2z * iT * t - 6 * p1z * iT * t + 3 * p1z * iT2 - 3 * p0z * iT2) + 2 * sqr(3 * p3y * t2 - 3 * p2y * t2 + 6 * p2y * iT * t - 6 * p1y * iT * t + 3 * p1y * iT2 - 3 * p0y * iT2) + 2 * sqr(3 * p3x * t2 - 3 * p2x * t2 + 6 * p2x * iT * t - 6 * p1x * iT * t + 3 * p1x * iT2 - 3 * p0x * iT2) + 2 * (6 * p3z * t - 12 * p2z * t + 6 * p1z * t + 6 * p2z * iT - 12 * p1z * iT + 6 * p0z * iT) * (p3z * t3 + 3 * p2z * iT * t2 + 3 * p1z * iT2 * t + p0z * iT3) + 2 * (6 * p3y * t - 12 * p2y * t + 6 * p1y * t + 6 * p2y * iT - 12 * p1y * iT + 6 * p0y * iT) * (p3y * t3 + 3 * p2y * iT * t2 + 3 * p1y * iT2 * t + p0y * iT3) + 2 * (6 * p3x * t - 12 * p2x * t + 6 * p1x * t + 6 * p2x * iT - 12 * p1x * iT + 6 * p0x * iT) * (p3x * t3 + 3 * p2x * iT * t2 + 3 * p1x * iT2 * t + p0x * iT3))) / (2 * System.Math.Pow(sqr(p3z * t3 + 3 * p2z * iT * t2 + 3 * p1z * iT2 * t + p0z * iT3) + sqr(p3y * t3 + 3 * p2y * iT * t2 + 3 * p1y * iT2 * t + p0y * iT3) + sqr(p3x * t3 + 3 * p2x * iT * t2 + 3 * p1x * iT2 * t + p0x * iT3), 3.0 / 2.0)) - ((rz * (3 * p3z * t2 - 3 * p2z * t2 + 6 * p2z * iT * t - 6 * p1z * iT * t + 3 * p1z * iT2 - 3 * p0z * iT2) + ry * (3 * p3y * t2 - 3 * p2y * t2 + 6 * p2y * iT * t - 6 * p1y * iT * t + 3 * p1y * iT2 - 3 * p0y * iT2) + rx * (3 * p3x * t2 - 3 * p2x * t2 + 6 * p2x * iT * t - 6 * p1x * iT * t + 3 * p1x * iT2 - 3 * p0x * iT2)) * (2 * (3 * p3z * t2 - 3 * p2z * t2 + 6 * p2z * iT * t - 6 * p1z * iT * t + 3 * p1z * iT2 - 3 * p0z * iT2) * (p3z * t3 + 3 * p2z * iT * t2 + 3 * p1z * iT2 * t + p0z * iT3) + 2 * (3 * p3y * t2 - 3 * p2y * t2 + 6 * p2y * iT * t - 6 * p1y * iT * t + 3 * p1y * iT2 - 3 * p0y * iT2) * (p3y * t3 + 3 * p2y * iT * t2 + 3 * p1y * iT2 * t + p0y * iT3) + 2 * (3 * p3x * t2 - 3 * p2x * t2 + 6 * p2x * iT * t - 6 * p1x * iT * t + 3 * p1x * iT2 - 3 * p0x * iT2) * (p3x * t3 + 3 * p2x * iT * t2 + 3 * p1x * iT2 * t + p0x * iT3))) / System.Math.Pow(sqr(p3z * t3 + 3 * p2z * iT * t2 + 3 * p1z * iT2 * t + p0z * iT3) + sqr(p3y * t3 + 3 * p2y * iT * t2 + 3 * p1y * iT2 * t + p0y * iT3) + sqr(p3x * t3 + 3 * p2x * iT * t2 + 3 * p1x * iT2 * t + p0x * iT3), 3.0 / 2.0) + (3 * (rz * (p3z * t3 + 3 * p2z * iT * t2 + 3 * p1z * iT2 * t + p0z * iT3) + ry * (p3y * t3 + 3 * p2y * iT * t2 + 3 * p1y * iT2 * t + p0y * iT3) + rx * (p3x * t3 + 3 * p2x * iT * t2 + 3 * p1x * iT2 * t + p0x * iT3)) * sqr(2 * (3 * p3z * t2 - 3 * p2z * t2 + 6 * p2z * iT * t - 6 * p1z * iT * t + 3 * p1z * iT2 - 3 * p0z * iT2) * (p3z * t3 + 3 * p2z * iT * t2 + 3 * p1z * iT2 * t + p0z * iT3) + 2 * (3 * p3y * t2 - 3 * p2y * t2 + 6 * p2y * iT * t - 6 * p1y * iT * t + 3 * p1y * iT2 - 3 * p0y * iT2) * (p3y * t3 + 3 * p2y * iT * t2 + 3 * p1y * iT2 * t + p0y * iT3) + 2 * (3 * p3x * t2 - 3 * p2x * t2 + 6 * p2x * iT * t - 6 * p1x * iT * t + 3 * p1x * iT2 - 3 * p0x * iT2) * (p3x * t3 + 3 * p2x * iT * t2 + 3 * p1x * iT2 * t + p0x * iT3))) / (4 * System.Math.Pow(sqr(p3z * t3 + 3 * p2z * iT * t2 + 3 * p1z * iT2 * t + p0z * iT3) + sqr(p3y * t3 + 3 * p2y * iT * t2 + 3 * p1y * iT2 * t + p0y * iT3) + sqr(p3x * t3 + 3 * p2x * iT * t2 + 3 * p1x * iT2 * t + p0x * iT3), 5.0 / 2.0));
+        }
+        private static double sqrt(double d) { return System.Math.Sqrt(d); }
+        private static double sqr(double d) { return d*d; }
         //Position(t) = (1-t)^3*Point0+3*(1-t)^2*t*Point1+3*(1-t)*t^2*Point2+t^3*Point3
         private static Vector3 GetPos(Vector3 p0,Vector3 p1,Vector3 p2,Vector3 p3,float t)
         {
             float iT = 1 - t;
             return iT * iT * iT * p0 + 3 * iT * iT * t * p1 + 3 * iT * t * t * p2 + t * t * t * p3;
-        }
-        private static Coefs GetCoefs(Vector3 p0,Vector3 p1,Vector3 p2,Vector3 p3,Vector3 r)
-        {
-            Coefs retr = new Coefs();
-            retr.t3 = (p3.z-3*p2.z+3*p1.z-p0.z)*r.z+(p3.y-3*p2.y+3*p1.y-p0.y)*r.y+(p3.x-3*p2.x+3*p1.x-p0.x)*r.x;
-            retr.t2 = (3*p2.z-6*p1.z+3*p0.z)*r.z+(3*p2.y-6*p1.y+3*p0.y)*r.y+(3*p2.x-6*p1.x+3*p0.x)*r.x;
-            retr.t1 = (3*p1.z-3*p0.z)*r.z+(3*p1.y-3*p0.y)*r.y+(3*p1.x-3*p0.x)*r.x;
-            retr.c = p0.z * r.z + p0.y * r.y + p0.x * r.x;
-            return retr;
-        }
-        private struct Coefs
-        {
-            public float t3;
-            public float t2;
-            public float t1;
-            public float c;
         }
     }
 }
