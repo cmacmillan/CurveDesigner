457fcc990f641cb82b8804f11f5f47ef3591b736
Chase MacMillan <chasemacmillan@hotmail.com>
2021-12-24 01:12:00 -0800

    BVH

---
+++ Scripts/Core/Curve3D_BVH.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ChaseMacMillan.CurveDesigner
+{
+    public class Curve3D_BVH
+    {
+        public static bool TestHitTriangle(Ray ray, Vector3 p0,Vector3 p2,Vector3 p3)
+        {
+            return true;
+        }
+        public static void RayBary(Ray ray, Vector3 p0,Vector3 p2,Vector3 p3, out float b0, out float b1, out float b2)
+        {
+            b0 = 0;
+            b1 = 0;
+            b2 = 0;
+        }
+    }
+}
